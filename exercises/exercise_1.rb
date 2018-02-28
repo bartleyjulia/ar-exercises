1b73af63a2c189d9cf2cc068ed4b5b03b1bb8363
Julia <bartleyjulia@gmail.com>
2018-02-28 01:57:22 +0000

    Exercise 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,7 +7,7 @@ class Store < ActiveRecord::Base
   belongs_to :stores
 end
 
-burnaby = Store.create(name: "Burnaby", annual_revenue: "300,000", mens_apparel: true, womens_apparel: true)
+burnaby = Store.create(name: "Burnaby", annual_revenue: "300_000", mens_apparel: true, womens_apparel: true)
 richmond = Store.create(name: "Richmond", annual_revenue: "1260000", mens_apparel: false, womens_apparel: true)
 gastown = Store.create(name: "Gastown", annual_revenue: "190000", mens_apparel: true, womens_apparel: false)
 
